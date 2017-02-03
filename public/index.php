6cceca5b7712df4bc1b22ecebfd399f1dd7fac9a
Anton <toh1napocht@yandex.ru>
2017-02-03 22:56:46 +0200

    catch errors

--- public/index.php
+++ public/index.php
@@ -6,7 +6,14 @@ define('APP_PATH', realpath('..'.DIRECTORY_SEPARATOR.'app'));
 define('MODULES_PATH', APP_PATH.DIRECTORY_SEPARATOR.'modules');
 define('CONFIG_PATH', APP_PATH.DIRECTORY_SEPARATOR.'config');
 
-/** @var \Phalcon\Mvc\Micro $app */
-$app = require_once APP_PATH.DIRECTORY_SEPARATOR.'app.php';
+try{
+    /** @var \Phalcon\Mvc\Micro $app */
+    $app = require_once APP_PATH.DIRECTORY_SEPARATOR.'app.php';
 
-$app->handle();
+    $app->handle();
+}catch (\Exception $e){
+    $exception = new \Rest\Components\RestException(500, [
+        'devMessage'=>$e->getMessage()
+    ]);
+    $exception->send();
+}
