0ac4ab86bea445a89464b0421ffbda673434bc58
Anton <toh1napocht@yandex.ru>
2017-02-04 17:54:39 +0200

    add fake auth control

--- app/app.php
+++ app/app.php
@@ -2,7 +2,6 @@
 
 use Phalcon\DI\FactoryDefault as DI;
 
-
 /**
  * The DI is our direct injector.  It will store pointers to all of our services
  * and we will insert it into all of our controllers.
@@ -36,6 +35,33 @@ if($di->has('collections')){
             $app->mount($collection);
     }
 }
+
+/**
+ * Before all requests check Bearer token and auth, or not
+ */
+$app->before(function() use($app, $di){
+    $allow_requests = ['/v1/auth'];
+    if(in_array($app->request->getURI(), $allow_requests)){
+        return true;
+    }
+
+    $authHeader = $app->request->getHeader('Authorization');
+    if ($authHeader !== null && preg_match('/^Bearer\s+(.*?)$/', $authHeader, $matches)) {
+        //check $matches[1] (token)
+        //get user, and set like service
+        //if valid
+        /* TODO: move auth methods to classes, and add opportunity override in the modules*/
+        return true;
+    }
+
+    throw new \Rest\Components\RestException(
+        401,
+        'Need authorized',
+        [
+            'devCode' => 'BEFORE1'
+        ]
+    );
+});
 
 /**
  * Show error or list of defined routes
