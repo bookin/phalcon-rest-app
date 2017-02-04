d1de5524f62f2d2da942b0b47893718a4405a591
Anton <toh1napocht@yandex.ru>
2017-02-04 18:52:41 +0200

    global path to public folder

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ defined('APP_ENV') or define('APP_ENV', 'dev');
 define('APP_PATH', realpath('..'.DIRECTORY_SEPARATOR.'app'));
 define('MODULES_PATH', APP_PATH.DIRECTORY_SEPARATOR.'modules');
 define('CONFIG_PATH', APP_PATH.DIRECTORY_SEPARATOR.'config');
+define('PUBLIC_PATH', __DIR__);
 
 try{
     /** @var \Phalcon\Mvc\Micro $app */
